d45a5891f7ce59a71e5b9010737e0ece3dda40a1
KHANN Sovannaphom <sovannaphom.khann@sbilhbank.com.kh>
2024-06-20 10:41:20 +0700

    wip

--- app/src/main/java/com/example/foodapp/activites/MealActivity.kt
+++ app/src/main/java/com/example/foodapp/activites/MealActivity.kt
@@ -1,8 +1,11 @@
 package com.example.foodapp.activites
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
@@ -22,6 +25,7 @@ class MealActivity : AppCompatActivity() {
     private lateinit var mealThumb: String
     private lateinit var binding: ActivityMealBinding
     private lateinit var mealMvvm: MealViewModel
+    private lateinit var youtubeLink: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,9 @@ class MealActivity : AppCompatActivity() {
         getMealInformationFromIntent()
         setInformationIntentView()
 
+        //call loading case when we call to api
+        loadingCase()
+
         mealMvvm.getMealDetail(mealId)
         observeMealDetailsLiveData()
 
@@ -43,17 +50,33 @@ class MealActivity : AppCompatActivity() {
         //enable the up button
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        //Click image youtube to navigate to youtube
+        onYoutubeImgClick()
+
     }
 
+    private fun onYoutubeImgClick(){
+        binding.imgYoutube.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
+    }
+
+    //handle clicked on img youtube
     @SuppressLint("SetTextI9n")
     private fun observeMealDetailsLiveData() {
         mealMvvm.observeMealDetailsLiveData().observe(this, object : Observer<Meal> {
             @SuppressLint("SetTextI18n")
             override fun onChanged(value: Meal) {
+                respondCase()   //when got respond from API
                 val meal = value
                 binding.tvCategory.text = "Category : ${meal.strCategory}"
                 binding.tvArea.text = "Area : ${meal.strArea}"
                 binding.tvInstructionSt.text = meal.strInstructions
+
+                //Assigns the YouTube link from the Meal object to the youtubeLink variable.
+                // Update the YouTube link variable with the new link from the meal
+                youtubeLink = meal.strYoutube
             }
         })
     }
@@ -75,6 +98,39 @@ class MealActivity : AppCompatActivity() {
         mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
     }
 
+    // Handle the Up button press
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
+    //loading case
+    private fun loadingCase() {
+
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnAddToFav.visibility = View.INVISIBLE
+        binding.tvInstructions.visibility = View.INVISIBLE
+        binding.tvCategory.visibility = View.INVISIBLE
+        binding.tvArea.visibility = View.INVISIBLE
+        binding.imgYoutube.visibility = View.INVISIBLE
+
+    }
+
+    //respond case
+    private fun respondCase() {
+
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.btnAddToFav.visibility = View.VISIBLE
+        binding.tvInstructions.visibility = View.VISIBLE
+        binding.tvCategory.visibility = View.VISIBLE
+        binding.tvArea.visibility = View.VISIBLE
+        binding.imgYoutube.visibility = View.VISIBLE
+
+    }
 
 }
